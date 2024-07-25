0e4e180d5ef537f2c8539db40e9d6f56368aa9d1
Umang Tiwari <umangtiwari52@gmail.com>
2024-07-25 16:30:41 +0530

    practiced using previous states

--- my-first-react-app/my-first-react-app/src/App.js
+++ my-first-react-app/my-first-react-app/src/App.js
@@ -3,19 +3,21 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  const [name,setName]=useState('Mario');
-  const handleClick=()=>{
-    setName('Umang Tiwari')
-  }
+  
+  const handleClick = (id)=>{
+		setEvents((preevents)=>{return preevents.filter((event)=>{
+		return id!=event.id})
+		})
+}
   const [events,setEvents]= useState([{title : "marios birthday’", id: 1},{title :" ‘bowser bha’",id : 2},{title :" ‘luigi’",id:3}]) 
   return (
     <div className="App">
-      <h1>{name}</h1>
-      <button onClick={handleClick}>click karne se naam badlega</button>
+
       {
         events.map((event, index)=>(
           <div key={event.id}>
             <h1 >{event.title}</h1>
+            <button onClick={()=>handleClick(event.id)}>clear event</button>
           </div>
         ))
       }
