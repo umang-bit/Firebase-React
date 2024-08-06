768e053eed9a29e5692cdfa268363c253bf9cf88
Umang Tiwari <umangtiwari52@gmail.com>
2024-08-06 13:48:14 +0530

    practiced onChange in React

--- my-first-react-app/my-first-react-app/src/Neweventform.js
+++ my-first-react-app/my-first-react-app/src/Neweventform.js
@@ -1,19 +1,29 @@
+import { useState } from "react"
 
 
 export default function Neweventform() {
+    const [titleInput,setTitleInput]=useState('')
+    const[dateInput,setDateInput]=useState('')
+    const handleChangetitle=(e)=>{
+        setTitleInput(e.target.value)
+    }
+    const handleChangedate=(e)=>{
+        setDateInput(e.target.value)
+    }
   return (
 
     <form>
         <label>
             <span>enter title</span>
-            <input type="text"></input>
+            <input type="text" onChange={handleChangetitle}></input>
         </label>
 
         <label>
             <span>enter date</span>
-            <input type="date"></input>
+            <input type="date" onChange={handleChangedate}></input>
         </label>
         <button>Submit</button>
+        <span>{titleInput}{dateInput}</span>
     </form>
   )
 }
