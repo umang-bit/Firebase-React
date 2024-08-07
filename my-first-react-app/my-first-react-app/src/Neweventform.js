2a3f8a6eb4d8c5eb6b0f51b78743c651fe6c4b8f
Umang Tiwari <umangtiwari52@gmail.com>
2024-08-07 18:16:08 +0530

    added location checkbox

--- my-first-react-app/my-first-react-app/src/Neweventform.js
+++ my-first-react-app/my-first-react-app/src/Neweventform.js
@@ -9,7 +9,9 @@ export default function Neweventform({addNewEvent}) {
         const event ={
             title : titleInput,
             date : dateInput,
+            location:location,
             id : Math.floor(Math.random()) * 10000
+
         }
         addNewEvent(event);//this is where we use the function we took form app.js
         handleReset();
@@ -17,6 +19,7 @@ export default function Neweventform({addNewEvent}) {
     const handleReset=()=>{
         setTitleInput('')
         setDateInput('')
+        setLocation('New Delhi')
     }
     const handleChangetitle=(e)=>{
         setTitleInput(e.target.value)
@@ -24,8 +27,9 @@ export default function Neweventform({addNewEvent}) {
     const handleChangedate=(e)=>{
         setDateInput(e.target.value)
     }
+    const [location,setLocation]=  useState('New Delhi')//new delhi will be the default location
   return (
-
+ 
     <form onSubmit={handleSubmit}>
         <label>
             <span>enter title</span>
@@ -35,6 +39,15 @@ export default function Neweventform({addNewEvent}) {
         <label>
             <span>enter date</span>
             <input type="date" onChange={handleChangedate} value={dateInput}></input>
+        </label>
+        <label>
+            <span>select location</span>
+            <select onChange={(e)=>setLocation(e.target.value)}>
+                <option value='New Delhi'></option>
+                <option value='Vadodara'></option>
+                <option value='Sharjah'></option>
+                <option value='Mumbai'></option>
+            </select>
         </label>
         <button>Submit</button>
         <span>{titleInput}{dateInput}</span>
