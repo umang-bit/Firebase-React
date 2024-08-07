a482d0c121a056c8d359e867db26094327fa6aa7
Umang Tiwari <umangtiwari52@gmail.com>
2024-08-07 16:11:55 +0530

    practiced controlled inputs

--- my-first-react-app/my-first-react-app/src/Neweventform.js
+++ my-first-react-app/my-first-react-app/src/Neweventform.js
@@ -4,6 +4,19 @@ import { useState } from "react"
 export default function Neweventform() {
     const [titleInput,setTitleInput]=useState('')
     const[dateInput,setDateInput]=useState('')
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const event ={
+            title : titleInput,
+            date : dateInput,
+            id : Math.floor(Math.random()) * 10000
+        }
+        handleReset();
+    }
+    const handleReset=()=>{
+        setTitleInput('')
+        setDateInput('')
+    }
     const handleChangetitle=(e)=>{
         setTitleInput(e.target.value)
     }
@@ -12,18 +25,19 @@ export default function Neweventform() {
     }
   return (
 
-    <form>
+    <form onSubmit={handleSubmit}>
         <label>
             <span>enter title</span>
-            <input type="text" onChange={handleChangetitle}></input>
+            <input type="text" onChange={handleChangetitle} value={titleInput}></input>
         </label>
 
         <label>
             <span>enter date</span>
-            <input type="date" onChange={handleChangedate}></input>
+            <input type="date" onChange={handleChangedate} value={dateInput}></input>
         </label>
         <button>Submit</button>
         <span>{titleInput}{dateInput}</span>
+        <p onClick={handleReset}>Reset Karo</p>    
     </form>
   )
 }
