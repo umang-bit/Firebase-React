f81361b808a3e6cc34aebe75e0f6e55308af1680
Umang Tiwari <umangtiwari52@gmail.com>
2024-08-10 17:01:07 +0530

    added filter for trips using dependency array in useEffect hook

--- tripago/src/Triplist.js
+++ tripago/src/Triplist.js
@@ -3,9 +3,11 @@ import React, { useState,useEffect } from 'react'
 
 export default function Triplist() {
     const [trips,setTrips]=useState([])//initially we keep it an empty array
-useEffect(()=>{fetch('http://localhost:3000/trips')
+    const [url,seturl]=useState('http://localhost:3000/trips')
+
+useEffect(()=>{fetch(url)
     .then((response)=>response.json())
-    .then((data)=>setTrips(data))},[])
+    .then((data)=>setTrips(data))},[url])
   return (
     <div className='trip-list'>
       <h2>Trip List</h2>
@@ -20,7 +22,10 @@ useEffect(()=>{fetch('http://localhost:3000/trips')
 
         )})}
         </ul>
-      
+        <div className='filters'>Filters  
+            <button onClick={()=>seturl('http://localhost:3000/trips?loc=europe')}>Europe Trips</button>
+            <button onClick={()=>seturl('http://localhost:3000/trips')}>Other Trips</button>
+        </div>  
     </div>
   )
 }
